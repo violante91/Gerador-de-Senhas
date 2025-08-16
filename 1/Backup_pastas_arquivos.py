40ecb4352041bc208d46c1667214e38a10563ac2
violante91 <violantejunior@gmail.com>
2025-08-15 22:29:57 -0300

    finale

--- 1/Backup_pastas_arquivos.py
+++ 1/Backup_pastas_arquivos.py
@@ -1,29 +1,37 @@
-# Janela para selecionar a pasta do nosso computador
 import os
 from tkinter.filedialog import askdirectory
 import shutil
 import datetime
 
+# Seleciona a pasta do computador
 nome_pasta_selecionada = askdirectory()
 
-lista_arquivos = os.listdir(nome_pasta_selecionada)
+# Lista os arquivos e pastas da pasta selecionada, excluindo a pasta de backup
+lista_arquivos = [f for f in os.listdir(nome_pasta_selecionada) if f != 'backup']
 
-# Fazer o backup dedos arquivos que estão na pasta selecionada
+# Define o caminho da pasta de backup
 nome_pasta_backup = 'backup'
-nome_completo_pasta_backup = f'{nome_pasta_selecionada}/{nome_pasta_backup}'
+nome_completo_pasta_backup = os.path.join(nome_pasta_selecionada, nome_pasta_backup)
+
+# Cria a pasta de backup caso não exista
 if not os.path.exists(nome_completo_pasta_backup):
     os.mkdir(nome_completo_pasta_backup)
 
+# Define a pasta de backup atual com data e hora
 data_atual = datetime.datetime.today().strftime('%Y-%m-%d %H.%M.%S')
+pasta_backup_atual = os.path.join(nome_completo_pasta_backup, data_atual)
 
-for arquivo in lista_arquivos:    
-    nome_completo_arquivo =f'{nome_pasta_selecionada}/{arquivo}'
-    nome_final_arquivo = f'{nome_completo_pasta_backup}/{data_atual}/{arquivo}'
+# Cria a pasta de backup da data atual
+if not os.path.exists(pasta_backup_atual):
+    os.mkdir(pasta_backup_atual)
 
-if not os.path.exists(f'{nome_completo_pasta_backup}/{data_atual}'):
-    os.mkdir(f'{nome_completo_pasta_backup}/{data_atual}')
+# Copia arquivos e pastas para a pasta de backup
+for item in lista_arquivos:
+    print(item)
+    caminho_item = os.path.join(nome_pasta_selecionada, item)
+    destino_item = os.path.join(pasta_backup_atual, item)
 
-    if '.' in arquivo:
-        shutil.copy2(nome_completo_arquivo, nome_final_arquivo)   
-    elif 'backup' not in arquivo:
-       shutil.copytree(nome_completo_arquivo, nome_final_arquivo) 
+    if os.path.isfile(caminho_item):
+        shutil.copy2(caminho_item, destino_item)
+    elif os.path.isdir(caminho_item):
+        shutil.copytree(caminho_item, destino_item)
